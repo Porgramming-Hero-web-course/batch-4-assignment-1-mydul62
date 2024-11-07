6cb738ba52601d57a62e2a9d266a8815db212c6f
unknown <mydulcse62.niter@gmail.com>
2024-11-07 15:39:22 +0600

    added solution of problem6

---
+++ src/problem-6.ts
@@ -0,0 +1,21 @@
+interface IMyProfile {
+  name: string;
+  age: number;
+  email: string;
+}
+
+const myProfile: IMyProfile = { 
+  name: "Alice",
+  age: 25,
+  email: "alice@example.com"
+};
+
+function updateProfile(myProfile: IMyProfile, updateInfo: Partial<IMyProfile>) {
+  const updatedProfile = {
+    ...myProfile,
+    ...updateInfo,
+  };
+  return updatedProfile;
+}
+
+console.log(updateProfile(myProfile, { age: 28,name: "mahim"}));
